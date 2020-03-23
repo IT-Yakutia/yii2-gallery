b251d762ca857bfc32cdffeb3f8cb8ed2883fdc9
PlayerZeroOne <general.dmf@gmail.com>
2020-03-23 23:17:37 +0300

    return types deleted

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -8,7 +8,7 @@ use yii\base\BootstrapInterface;
 
 class Bootstrap implements BootstrapInterface
 {
-    public function bootstrap($app): void
+    public function bootstrap($app)
     {
         $app->setModule('gallery', 'ityakutia\gallery\Module');
     }
